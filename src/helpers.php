ff2a37156dc00780636061cbf7b9a700c6135d38
pitsolu <pitsolu@gmail.com>
2024-01-25 05:20:28 +0300

    added strukt/base

--- src/helpers.php
+++ src/helpers.php
@@ -2,38 +2,53 @@
 
 use Strukt\Queue;
 
-function push(string $queue_name, array $item){
+helper("queue");
 
-	$mq = new Queue();
-	return $mq->push($queue_name, $item);
-};
+if(helper_add("push")){
 
-function pop(string $queue_name){
+	function push(string $queue_name, array $item){
 
-	$mq = new Queue();
-	$data = $mq->pop($queue_name, $id);
-
-	return [$id, $data];
-};
-
-function reply(string $queue_name, callable $func){
-
-	$mq = new Queue();
-	$data = $mq->pop($queue_name, $mid);
-
-	$reply = call_user_func($func, $data);
-	
-	return $mq->reply($queue_name, $mid, $reply);
+		$mq = new Queue();
+		return $mq->push($queue_name, $item);
+	};
 }
 
-function wait(string $queue_name, array $data, callable $func){
+if(helper_add("pop")){
 
-	$mq = new Queue();
-	$mid = $mq->push($queue_name, $data);
-	while(!$mq->is_processed($queue_name, $mid))// Wait for execution
-		sleep(1);
+	function pop(string $queue_name){
 
-	$reply = $mq->getReply($queue_name, $mid);
+		$mq = new Queue();
+		$data = $mq->pop($queue_name, $id);
 
-	return call_user_func($func, $reply, $mid);
+		return [$id, $data];
+	};
+}
+
+
+if(helper_add("reply")){
+
+	function reply(string $queue_name, callable $func){
+
+		$mq = new Queue();
+		$data = $mq->pop($queue_name, $mid);
+
+		$reply = call_user_func($func, $data);
+		
+		return $mq->reply($queue_name, $mid, $reply);
+	}
+}
+
+if(helper_add("wait")){
+
+	function wait(string $queue_name, array $data, callable $func){
+
+		$mq = new Queue();
+		$mid = $mq->push($queue_name, $data);
+		while(!$mq->is_processed($queue_name, $mid))// Wait for execution
+			sleep(1);
+
+		$reply = $mq->getReply($queue_name, $mid);
+
+		return call_user_func($func, $reply, $mid);
+	}
 }
